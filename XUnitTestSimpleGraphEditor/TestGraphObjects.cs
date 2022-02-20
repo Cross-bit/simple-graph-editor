e65586fc45edc9f985934c59aa2e00e07e0b680f
Cross-bit <ondra.kryz@gmail.com>
2022-02-21 00:20:50 +0100

    unit test for node comparision

--- XUnitTestSimpleGraphEditor/TestGraphObjects.cs
+++ XUnitTestSimpleGraphEditor/TestGraphObjects.cs
@@ -8,19 +8,27 @@ namespace XUnitTestSimpleGraphEditor
 {
     public class TestGraphObjects
     {
+        [Fact]
         public void TestNodesEquality() {
 
             // nodes should override equal comparision based on their coordinates
 
             var nd1 = new Node(0, 1, new NodeData());
             var nd2 = new Node(0, 1, new NodeData());
+            var nd3 = new Node(1, 0, new NodeData());
 
             // should be equal
             Assert.True(nd1.Equals(nd2));
+            Assert.False(nd1.Equals(nd3));
 
-            // test type
-            
+            // type comparision
+            Assert.False(nd1.Equals(new String("")));
 
+            Assert.True(nd1 == nd2);
+            Assert.False(nd1 != nd2);
+            Assert.False(nd1 == nd3);
+            Assert.True(nd1 != nd3);
         }
     }
 }
+
